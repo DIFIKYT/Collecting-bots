c5e1f6a20a1a4d66be21b8432b2aae2011d88634
DIFIKYT <68289492+DIFIKYT@users.noreply.github.com>
2024-09-12 13:40:26 +0700

    Initial commit

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    [SerializeField] private float _cameraSpeed;
+    [SerializeField] private float _edgeSize;
+
+    private float _screenWidth;
+    private float _screenHeight;
+
+    void Start()
+    {
+        _screenWidth = Screen.width;
+        _screenHeight = Screen.height;
+    }
+
+    void Update()
+    {
+        Vector3 movement = Vector3.zero;
+
+        if (Input.mousePosition.x <= _edgeSize)
+            movement.x = -_cameraSpeed * Time.deltaTime;
+        if (Input.mousePosition.x >= _screenWidth - _edgeSize)
+            movement.x = _cameraSpeed * Time.deltaTime;
+        if (Input.mousePosition.y >= _screenHeight - _edgeSize)
+            movement.z = _cameraSpeed * Time.deltaTime;
+        if (Input.mousePosition.y <= _edgeSize)
+            movement.z = -_cameraSpeed * Time.deltaTime;
+
+        transform.Translate(movement, Space.World);
+    }
+}
